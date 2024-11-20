4dc49719a11fe386cf5566325783e13edc9710fd
EC2 Default User <ec2-user@ip-172-31-35-214.eu-north-1.compute.internal>
2024-11-20 23:22:18 +0000

    11/21 8:22

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,17 +13,34 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    # if @book.save
+      # flash[:notice] = "You've uodated book successfully"
+      # redirect_to book_path(@book)
+    # else
+      # render :new
+    # end
   end
 
   def create
     @book = Book.new(book_params)
-    @book.user_id = current_user.id  # ログイン中のユーザーの ID を book の user_id に代入
+    @book.user_id = current_user.id
     if @book.save
-      redirect_to books_path, notice: "Book was successfully created."
+      flash[:notice] = "You've created book successfully"
+      redirect_to book_path(@book)
     else
-      render :index
-    @book.save
-    redirect_to book_path(@book)
+      render :new
+
+    # フラッシュメッセージ
+    # １. データを受け取り新規登録するためのインスタンス作成
+    # @list = List.new(list_params)
+    # 2. データをデータベースに保存するためのsaveメソッド実行
+    # if @list.save
+      # 3. フラッシュメッセージを定義し、詳細画面へリダイレクト
+      # flash[:notice] = "投稿に成功しました。"
+      # redirect_to list_path(@list.id)
+      # <div>
+      # <%= flash[:notice] %>
+    # </div>
     end
 end
 
@@ -35,12 +52,14 @@ end
 
   def update
     @book = Book.find(params[:id])
-    if @book.update(book_params)
-      redirect_to book_path(@book), notice: "Book was successfully updated."
-    else
-      render :edit
-    end
+  if @book.update(book_params)
+    flash[:notice] = "You've updated book successfully"
+    redirect_to book_path(@book)
+  else
+    render :edit
   end
+  end
+  
 
    # ストロングパラメータ
    def book_params

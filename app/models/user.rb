04f2106b37f841287e1b734a582f5ee7349a6778
EC2 Default User <ec2-user@ip-172-31-35-214.eu-north-1.compute.internal>
2024-11-20 16:23:10 +0000

    11/21 1:23

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,32 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+   # ↓userモデルにnameとpositionに加えて画像を扱うためのimageカラムが追記されたかのように扱うことができます。
+   has_one_attached :image
+
+
+   # ↓ユーザーの画像を表示する
+   has_one_attached :profile_image
+ 
+   def get_profile_image(width, height)
+     unless profile_image.attached?
+       file_path = Rails.root.join('app/assets/images/default-image.jpg')
+       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+     end
+     profile_image.variant(resize_to_limit: [100, 100]).processed
+   end
+ 
+ 
+       # ↓があることでbook.rbと1:Nの関係になる
+   has_many :post_images, dependent: :destroy
+ 
+ 
+   # UserモデルとBookモデルの間に1対多の関連付けを定義します。Userが多数のBookを持つ関係なので、Userモデルに`has_many :books`を追加し、Bookモデルには`belongs_to :user`を追加します。
+   has_many :books
+ 
+ # `User`モデルに`introduction`というカラムが存在しない場合、`introduction`メソッドを追加して値を返すように定義します。
+ def introduction
+ end
+ 
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,23 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about", to: "homes#about"
+  resources :users, only: [:index, :show, :edit, :update, :create]
+  resources :books, only: [:new, :index, :show, :create, :edit, :destroy, :update]
+
+  get 'users/edit'
+
+  # ↓ヘッダーのAboutリンクをつくりたくて
+  get 'home/about', to: 'homes#about'
+  get 'book/index', to: 'books#index
+
+  
+  Rails.application.routes.draw do
+  get 'about', to: 'homes#about', as: 'about'
+    
+  end
+
 end
+
+
+# application.html.erbで`about_path`を使用できるようにしたもの↓
+# 上記のように設定することで'/about'にアクセスしたときにhomesコントローラーのaboutアクションが呼ばれるようになり、名前付きルート'about_path'が生成されます。

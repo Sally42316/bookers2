9f2ac2440671dd93900d9f90c9d406c8ea5dde78
EC2 Default User <ec2-user@ip-172-31-35-214.eu-north-1.compute.internal>
2024-11-25 09:20:52 +0000

    11/25　favoriteのcontrollerを作った

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,7 @@
 class Favorite < ApplicationRecord
     belongs_to :user
-  belongs_to :book
+    belongs_to :book
+
+    # ↓これで一人１かいのいいね！制限ができる
+    validates :user_id, uniqueness: {scope: :book_id}
 end

f6292e126fcf94a363d6d8cec12c713e815770c8
EC2 Default User <ec2-user@ip-172-31-35-214.eu-north-1.compute.internal>
2024-11-25 05:32:53 +0000

    11/25 いいね機能をつけようとしたらコメントも表示されなくなった

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,11 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   
   # ↓これでコメントが1:Nでつくようになる
-  has_many :book_comments, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  #  has_many :book_comments, dependent: :destroy では？？？
+
+  # ↓これでいいね機能がつかえる
+  # has_many :favorites, dependent: :destroy
 
    # ↓ユーザーの画像を表示する
   #  has_one_attached :profile_image
